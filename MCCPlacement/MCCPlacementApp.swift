e0eb712c4286047d2528785b616a0cdd24f18131
Kenneth Chew <kenneth.c0@protonmail.com>
2020-10-18 01:14:45 -0400

    Initial Commit

---
+++ MCCPlacement/MCCPlacementApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCCPlacementApp.swift
+//  MCCPlacement
+//
+//  Created by Kenneth Chew on 10/18/20.
+//
+
+import SwiftUI
+
+@main
+struct MCCPlacementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
